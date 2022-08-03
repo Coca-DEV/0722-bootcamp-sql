e2248a7ad839400485238bf00b48daaa3968b8c9
Jose H Coca <jhfcoca@hotmail.com>
2022-08-03 14:02:56 -0300

    engine

--- csvms/engine.py
+++ csvms/engine.py
@@ -1,6 +1,8 @@
 """CSVMS SQL Engine Module
 See https://github.com/Didone/csvms/discussions/6
 """
+from mo_sql_parsing import parse
+from csvms.table import Table
 
 class Engine():
     """Class used to implement bootcamp tasks"""
@@ -8,5 +10,19 @@ class Engine():
     def execute(self, sql:str):
         """Execute SQL statement
         :param sql: String with sql statement"""
-        #TODO Implement your SQL engine
-        raise NotImplementedError
+        ast = parse(sql)
+        if ast.get('create table') is not None:
+            self._create_table(
+                name = ast['create table']['name'],
+                columns = ast['create table']['columns']
+            )
+        elif ast.get('drop') is not None:
+                self._drop_table(
+                    name = ast['drop']['table']
+                )
+
+    def _create_table(self, name: str, columns: list):
+        print()
+
+    def _drop_table(self, name: str):
+        print()
